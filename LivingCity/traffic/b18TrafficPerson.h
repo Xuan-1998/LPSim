c4da29503ea3f3c8f9b9df71c495dec3d1c23bdd
Ubuntu <ubuntu@ip-172-31-39-199.us-east-2.compute.internal>
2023-01-30 08:14:23 +0000

    adding speed, edge id and travel time

--- LivingCity/traffic/b18TrafficPerson.h
+++ LivingCity/traffic/b18TrafficPerson.h
@@ -16,9 +16,13 @@ namespace LC {
 struct B18TrafficPerson {
   unsigned int init_intersection;
   unsigned int end_intersection;
+  unsigned int window_flag = 0;
   float time_departure;
   float dist_traveled = 0;
   float last_time_simulated = 0;
+  double avg_speed[10];
+  unsigned int prevEdge[10];
+  double travel_time[10];
 
   unsigned short active;//0 inactive 1 active 2 finished
   unsigned short numOfLaneInEdge;//number of lane in that edge
