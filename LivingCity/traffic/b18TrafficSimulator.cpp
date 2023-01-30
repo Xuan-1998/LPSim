06703780b7678007d28bc988804b71d798a16496
Ubuntu <ubuntu@ip-172-31-39-199.us-east-2.compute.internal>
2023-01-30 08:42:52 +0000

    某些trip到某些edge的时间

--- LivingCity/traffic/b18TrafficSimulator.cpp
+++ LivingCity/traffic/b18TrafficSimulator.cpp
@@ -2473,7 +2473,7 @@ void writePeopleFile(
   if (peopleFile.open(QIODevice::ReadWrite | QIODevice::Truncate)) {
     std::cout << "> Saving People file... (size " << trafficPersonVec.size() << ")" << std::endl;
     QTextStream streamP(&peopleFile);
-    streamP << "p,init_intersection,end_intersection,time_departure,num_steps,co,avg_speed1,avg_speed2,avg_speed3,avg_speed4,avg_speed5,avg_speed6,avg_speed7,prevEdge1,prevEdge2,prevEdge3,prevEdge4,prevEdge5,prevEdge6,prevEdge7,travel_time1, travel_time2,travel_time3,travel_time4,travel_time5,travel_time6,travel_time7,gas,distance,a,b,T,avg_v(mph),active,last_time_simulated,path_length_cpu,path_length_gpu\n";
+    streamP << "p,init_intersection,end_intersection,time_departure,num_steps,co,avg_speed1,avg_speed2,avg_speed3,avg_speed4,avg_speed5,avg_speed6,avg_speed7,prevEdge1,prevEdge2,prevEdge3,prevEdge4,prevEdge5,prevEdge6,prevEdge7,travel_time1, travel_time2,travel_time3,travel_time4,travel_time5,travel_time6,travel_time7,endtimeonprevEdge1,endtimeonprevEdge2, endtimeonprevEdge3, endtimeonprevEdge4, endtimeonprevEdge5, endtimeonprevEdge6, endtimeonprevEdge7,gas,distance,a,b,T,avg_v(mph),active,last_time_simulated,path_length_cpu,path_length_gpu\n";
 
     for (int p = 0; p < trafficPersonVec.size(); p++) {
       streamP << p;
@@ -2503,6 +2503,13 @@ void writePeopleFile(
 			streamP << "," << trafficPersonVec[p].travel_time[4];
 			streamP << "," << trafficPersonVec[p].travel_time[5];
 			streamP << "," << trafficPersonVec[p].travel_time[6];
+      streamP << "," << trafficPersonVec[p].end_time_on_prev_edge_array[0];
+			streamP << "," << trafficPersonVec[p].end_time_on_prev_edge_array[1];
+			streamP << "," << trafficPersonVec[p].end_time_on_prev_edge_array[2];
+			streamP << "," << trafficPersonVec[p].end_time_on_prev_edge_array[3];
+			streamP << "," << trafficPersonVec[p].end_time_on_prev_edge_array[4];
+			streamP << "," << trafficPersonVec[p].end_time_on_prev_edge_array[5];
+			streamP << "," << trafficPersonVec[p].end_time_on_prev_edge_array[6];
       streamP << "," << trafficPersonVec[p].gas;
       streamP << "," << trafficPersonVec[p].dist_traveled;
       streamP << "," << trafficPersonVec[p].a;
