3e9c34202f325f8168072555fab7a736ec4b5998
Xuan-1998 <j503440616@berkeley.edu>
2023-01-30 22:53:42 -0800

    only output travel time

--- LivingCity/traffic/b18TrafficSimulator.cpp
+++ LivingCity/traffic/b18TrafficSimulator.cpp
@@ -2474,41 +2474,41 @@ void writePeopleFile(
   if (peopleFile.open(QIODevice::ReadWrite | QIODevice::Truncate)) {
     std::cout << "> Saving People file... (size " << trafficPersonVec.size() << ")" << std::endl;
     QTextStream streamP(&peopleFile);
-    streamP << "p,init_intersection,end_intersection,time_departure,num_steps,co,avg_speed,prevEdge,travel_time,endtimeonprevEdge,gas,distance,a,b,T,avg_v(mph),active,last_time_simulated,path_length_cpu,path_length_gpu\n";
-
-    for (int p = 0; p < trafficPersonVec.size(); p++) {
-      int n = sizeof(trafficPersonVec[p].avg_speed)/sizeof(trafficPersonVec[p].avg_speed[0]);
-      QString str = "";
-      for (int i = 0; i < n; i++) {
-        str += QString::fromStdString(to_string(trafficPersonVec[p].avg_speed[i])) + QString::fromStdString(" ");
-      }
-      int n1 = sizeof(trafficPersonVec[p].prevEdge_array)/sizeof(trafficPersonVec[p].prevEdge_array[0]);
-      QString str1 = "";
-      for (int i = 0; i < n1; i++) {
-              str1 += QString::fromStdString(to_string(trafficPersonVec[p].prevEdge_array[i])) + QString::fromStdString(" ");
-          }
+//     streamP << "p,init_intersection,end_intersection,time_departure,num_steps,co,avg_speed,prevEdge,travel_time,endtimeonprevEdge,gas,distance,a,b,T,avg_v(mph),active,last_time_simulated,path_length_cpu,path_length_gpu\n";
+    streamP << "p,init_intersection,end_intersection,time_departure,num_steps,co,travel_time,gas,distance,a,b,T,avg_v(mph),active,last_time_simulated,path_length_cpu,path_length_gpu\n";
+//     for (int p = 0; p < trafficPersonVec.size(); p++) {
+//       int n = sizeof(trafficPersonVec[p].avg_speed)/sizeof(trafficPersonVec[p].avg_speed[0]);
+//       QString str = "";
+//       for (int i = 0; i < n; i++) {
+//         str += QString::fromStdString(to_string(trafficPersonVec[p].avg_speed[i])) + QString::fromStdString(" ");
+//       }
+//       int n1 = sizeof(trafficPersonVec[p].prevEdge_array)/sizeof(trafficPersonVec[p].prevEdge_array[0]);
+//       QString str1 = "";
+//       for (int i = 0; i < n1; i++) {
+//               str1 += QString::fromStdString(to_string(trafficPersonVec[p].prevEdge_array[i])) + QString::fromStdString(" ");
+//           }
       int n2 = sizeof(trafficPersonVec[p].travel_time)/sizeof(trafficPersonVec[p].travel_time[0]);
 
       QString str2 = "";
       for (int i = 0; i < n2; i++) {
               str2 += QString::fromStdString(to_string(trafficPersonVec[p].travel_time[i])) + QString::fromStdString(" ");
           }
-      int n3 = sizeof(trafficPersonVec[p].end_time_on_prev_edge_array)/sizeof(trafficPersonVec[p].end_time_on_prev_edge_array[0]);
+//       int n3 = sizeof(trafficPersonVec[p].end_time_on_prev_edge_array)/sizeof(trafficPersonVec[p].end_time_on_prev_edge_array[0]);
 
-      QString str3 = "";
-      for (int i = 0; i < n3; i++) {
-          str3 += QString::fromStdString(to_string(trafficPersonVec[p].end_time_on_prev_edge_array[i])) + QString::fromStdString(" ");
-          }
+//       QString str3 = "";
+//       for (int i = 0; i < n3; i++) {
+//           str3 += QString::fromStdString(to_string(trafficPersonVec[p].end_time_on_prev_edge_array[i])) + QString::fromStdString(" ");
+//           }
       streamP << p;
       streamP << "," << graph_->nodeIndex_to_osmid_[trafficPersonVec[p].init_intersection];
       streamP << "," << graph_->nodeIndex_to_osmid_[trafficPersonVec[p].end_intersection];
       streamP << "," << trafficPersonVec[p].time_departure;
       streamP << "," << trafficPersonVec[p].num_steps * deltaTime;
       streamP << "," << trafficPersonVec[p].co;
-      streamP << "," << str;
-      streamP << "," << str1;
+//       streamP << "," << str;
+//       streamP << "," << str1;
       streamP << "," << str2;
-      streamP << "," << str3;
+//       streamP << "," << str3;
 
 
 
